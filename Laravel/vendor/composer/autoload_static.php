38befcc2876cab7ca25c98192f163d724e42c1cb
Kamil Gabrysiewicz <kamilgabrysiewicz60@gmail.com>
2025-01-17 19:26:25 +0100

    Update: Some tweaks to docker-compose

--- Laravel/vendor/composer/autoload_static.php
+++ Laravel/vendor/composer/autoload_static.php
@@ -340,8 +340,8 @@ class ComposerStaticInit33817e7d71600911d1058814b9b70448
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- Laravel/vendor/composer/installed.php
+++ Laravel/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '615a551f4a9ae3aa5a8319e0c062ef23a6002ed5',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -371,9 +371,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '615a551f4a9ae3aa5a8319e0c062ef23a6002ed5',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
